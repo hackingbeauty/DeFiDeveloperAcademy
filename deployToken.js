2f24ddadef2436147a3d8005fcca3e1e1f4428fc
Mark Muskardin <markmuskardin@gmail.com>
2024-12-04 11:40:21 -0800

    added NonStandardERC20 token, associated tests, and deployment script

---
+++ deployToken.js
@@ -0,0 +1,18 @@
+const main = async() => {
+    const initialSupply = ethers.utils.parseEther("100000");
+    
+    const [deployer] = await ethers.getSigners();
+    console.log(`Address deploying the contract --> ${deployer.address}`);
+    
+    const tokenFactory = await ethers.getContractFactory("Token");
+    const contract = await tokenFactory.deploy(initialSupply);
+  
+    console.log(`Token Contract address --> ${contract.address}`);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+     .catch((error) => {
+        console.error(error);
+        process.exit(1);
+     });
