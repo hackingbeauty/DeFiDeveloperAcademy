25ff504d90b66c1ec0874178f699403f940bd687
Mark Muskardin <markmuskardin@gmail.com>
2024-12-08 10:27:10 -0800

    fixes

--- NonStandardERC20TokenTests.js
+++ NonStandardERC20TokenTests.js
@@ -14,7 +14,7 @@ describe("NonStandardERC20Token.sol", () => {
 
     beforeEach(async () => {
         [owner, alice, bob] = await ethers.getSigners();
-        initialSupply = ethers.utils.parseEther("100000");
+        initialSupply = ethers.parseEther("100000");
         contractFactory = await ethers.getContractFactory("NonStandardERC20Token");
         contract = await contractFactory.deploy(initialSupply);
         ownerAddress = await owner.getAddress();
@@ -29,31 +29,31 @@ describe("NonStandardERC20Token.sol", () => {
         });
         it("should have correct supply", async () => {
             const supply = await contract.getTotalSupply();
-            expect(ethers.utils.formatEther(supply)).to.equal(ethers.utils.formatEther(initialSupply));
+            expect(ethers.formatEther(supply)).to.equal(ethers.formatEther(initialSupply));
         });
         it("owner should have all the supply", async () => {
             const ownerBalance = await contract.balanceOf(ownerAddress);
-            expect(ethers.utils.formatEther(ownerBalance)).to.equal(ethers.utils.formatEther(initialSupply));
+            expect(ethers.formatEther(ownerBalance)).to.equal(ethers.formatEther(initialSupply));
         });
     });
 
     describe("Core", () => {
         it("owner should transfer to Alice and update balances", async () => {
-            const transferAmount = ethers.utils.parseEther("1000");
+            const transferAmount = ethers.parseEther("1000");
             let aliceBalance = await contract.balanceOf(aliceAddress);
-            expect(ethers.utils.formatEther(aliceBalance)).to.equal("0.0");
+            expect(ethers.formatEther(aliceBalance)).to.equal("0.0");
             await contract.transfer(transferAmount, aliceAddress);
             aliceBalance = await contract.balanceOf(aliceAddress);
-            expect(ethers.utils.formatEther(aliceBalance)).to.equal(ethers.utils.formatEther(transferAmount));
+            expect(ethers.formatEther(aliceBalance)).to.equal(ethers.formatEther(transferAmount));
         });
         it("owner should transfer to Alice and Alice to Bob", async () => {
-            const transferAmount = ethers.utils.parseEther("1000");
+            const transferAmount = ethers.parseEther("1000");
             await contract.transfer(transferAmount, aliceAddress); // contract is connected to the owner.
             let bobBalance = await contract.balanceOf(bobAddress);
-            expect(ethers.utils.formatEther(bobBalance)).to.equal("0.0");
+            expect(ethers.formatEther(bobBalance)).to.equal("0.0");
             await contract.connect(alice).transfer(transferAmount, bobAddress);
             bobBalance = await contract.balanceOf(bobAddress);
-            expect(ethers.utils.formatEther(bobBalance)).to.equal(ethers.utils.formatEther(transferAmount));
+            expect(ethers.formatEther(bobBalance)).to.equal(ethers.formatEther(transferAmount));
         });
     });
 });
