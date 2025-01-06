dc899a87b50020c8d281f9ae0d264e96c777dc38
Mark Muskardin <markmuskardin@gmail.com>
2025-01-06 08:52:49 -0800

    removed code for _mintFee() function

--- minimally-viable-dex-starter/contracts/core/TradingPair.sol
+++ minimally-viable-dex-starter/contracts/core/TradingPair.sol
@@ -73,21 +73,7 @@ contract TradingPair is ITradingPair, LiquidityTokenERC20 {
     }
 
     function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) {
-       address feeTo = IFactory(factory).feeTo();
-        feeOn = feeTo != address(0);
-        uint _kLast = kLast; // gas savings
-        if (feeOn) {
-            if (_kLast != 0) {
-                uint rootK = Math.sqrt(uint(_reserve0).mul(_reserve1));
-                uint rootKLast = Math.sqrt(_kLast);
-                if (rootK > rootKLast) {
-                    uint numerator = totalSupply.mul(rootK.sub(rootKLast));
-                    uint denominator = rootK.mul(5).add(rootKLast);
-                    uint liquidity = numerator / denominator;
-                    if (liquidity > 0) _mint(feeTo, liquidity);
-                }
-            }
-        }
+        // Add code for calculating the Accumulated Protocol Fee
     }
 
     function mint(address to) external lock returns (uint liquidity) {
