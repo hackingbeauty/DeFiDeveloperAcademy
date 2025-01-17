6b075ea298744dfa886c88e59dc3e2c80f7d30ac
Mark Muskardin <markmuskardin@gmail.com>
2025-01-16 16:20:20 -0800

    removed test deployment code in tests

--- minimally-viable-dex-starter/test/tests/periphery/Router.depositLiquidity.spec.js
+++ minimally-viable-dex-starter/test/tests/periphery/Router.depositLiquidity.spec.js
@@ -1,94 +1,12 @@
 const { expect } = require("chai");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { ethers, network } = require("hardhat");
-const { bigNumberSqrt } = require("../helpers/math-helpers.js");
-const BigNumber = require('bignumber.js');
+
 describe("Router contract", ()=> {
     
     describe("Deposit liquidity", () => {
         async function deployRouterFixture() {
-            await network.provider.send("hardhat_reset");
-    
-            /* AAVE/DAI, 1 AAVE = $56 USD, 1 DAI = $1 USD */
-            const amountADesired = ethers.utils.parseUnits('1', 18); //AAVE
-            const amountBDesired = ethers.utils.parseUnits('56', 18); //DAI
-            const amountAMin = ethers.utils.parseUnits('.99', 18); //AAVE
-            const amountBMin = ethers.utils.parseUnits('55.44', 18); //DAI
-    
-            const [deployer, liquidityProvider] = await ethers.getSigners();
-    
-            const FactoryContract = await ethers.getContractFactory("Factory");
-            const factory = await FactoryContract.deploy(deployer.address);
-            await factory.deployed();
-    
-            const WrappedETHContract = await ethers.getContractFactory("WETH");
-            const wethContract = await WrappedETHContract.deploy(deployer.address);
-            await wethContract.deployed();
-
-            const RouterContract = await ethers.getContractFactory("Router");
-            const deployedRouter = await RouterContract.deploy(factory.address, wethContract.address);
-            await deployedRouter.deployed();
-    
-            /* Connect router to signer */
-            const router = await deployedRouter.connect(liquidityProvider);
-    
-            const AaveTokenContract = await ethers.getContractFactory("ERC20Basic");
-            const aaveToken = await AaveTokenContract.deploy(
-                "Aave Stablecoin",
-                "AAVE",
-                18,
-                deployer.address
-            );
-            await aaveToken.deployed();
-    
-            const DaiTokenContract = await ethers.getContractFactory("ERC20Basic");
-            const daiToken = await DaiTokenContract.deploy(
-                "Dai Stablecoin",
-                "DAI",
-                18,
-                deployer.address
-            );
-            await daiToken.deployed();
-    
-            /* Mint tokens for Liquidity Provider's account */
-            await aaveToken.mint(
-                liquidityProvider.address,
-                ethers.utils.parseUnits('130', 18)
-            );
-    
-            await daiToken.mint(
-                liquidityProvider.address,
-                ethers.utils.parseUnits('130', 18)
-            );
-    
-            /* Liquidity Provider approves Router to transfer tokens */
-            await aaveToken.connect(liquidityProvider).approve(
-                router.address,
-                ethers.utils.parseUnits('130', 18)
-            );
-    
-            await daiToken.connect(liquidityProvider).approve(
-                router.address,
-                ethers.utils.parseUnits('130', 18)
-            );
-    
-            /* Transaction deadline of 20 minutes */
-            const currentTime = Math.floor(Date.now() / 1000); //divide by 1000 to get seconds
-            const deadline = currentTime + (20 * 60); //deadline is current time + 20 minutes
-    
-            return {
-                aaveToken,
-                daiToken,
-                amountADesired,
-                amountBDesired,
-                amountAMin,
-                amountBMin,
-                router,
-                liquidityProvider,
-                deadline,
-                factory,
-                deployer
-            }
+            
         }
         describe("should only allow a deposit of two ERC20 tokens of equal value", () => {
             
